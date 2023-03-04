3c2cbcd1985b07685e4ae7c364fdf6070839e2fc
mustafaguler3 <mustafa.glr@hotmail.com>
2023-03-04 18:30:14 +0300

    added article service & dto classes

--- BlogApp_Business/Abstract/IArticleService.cs
+++ BlogApp_Business/Abstract/IArticleService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BlogApp_Entities.Concrete;
+using BlogApp_Entities.Dtos;
+using BlogApp_Shared.Utilities.Results.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,14 @@ using System.Threading.Tasks;
 
 namespace BlogApp_Business.Abstract
 {
-    internal interface IArticleService
+    public interface IArticleService
     {
+        Task<IDataResult<Article>> Get(int articleId);
+        Task<IDataResult<IList<Article>>> GetAll();
+        Task<IDataResult<IList<Article>>> GetAllByNonDelete();
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName);
+        Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId, string modifiedByName);
+        Task<IResult> HardDelete(int articleId);
     }
 }
