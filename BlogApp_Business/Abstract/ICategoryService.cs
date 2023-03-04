70f51b2e8fbd986b8199696131da17de6c15aecb
mustafaguler3 <mustafa.glr@hotmail.com>
2023-03-04 16:40:26 +0300

    add service & dtos

--- BlogApp_Business/Abstract/ICategoryService.cs
+++ BlogApp_Business/Abstract/ICategoryService.cs
@@ -1,4 +1,6 @@
 ﻿using BlogApp_Entities.Concrete;
+using BlogApp_Entities.Dtos;
+using BlogApp_Shared.Utilities.Results.Abstract;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,11 @@ namespace BlogApp_Business.Abstract
 {
     public interface ICategoryService
     {
-        Task<IDataResult<Category>>
+        Task<IDataResult<Category>> Get(int categoryId);
+        Task<IDataResult<IList<Category>>> GetAll();
+        Task<IResult> Add(CategoryAddDto categoryAddDto,string createdByName);
+        Task<IResult> Update(CategoryUpdateDto categoryUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int categoryId);
+        Task<IResult> HardDelete(int categoryId);
     }
 }
---
+++ BlogApp_Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp_Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "Boş geçilemez")]
+        [MaxLength(70,ErrorMessage = "{0} karakterden büyük olamaz")]
+        [MinLength(3,ErrorMessage = "{0} karakterden az olmamalıdır.")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklama")]
+        [Required(ErrorMessage = "Boş geçilemez")]
+        [MaxLength(500, ErrorMessage = "{0} karakterden büyük olamaz")]
+        [MinLength(3, ErrorMessage = "{0} karakterden az olmamalıdır.")]
+        public string Description { get; set; }
+        [DisplayName("Not")]
+        public string Note { get; set; }
+        [DisplayName("Aktiv mi?")]
+        [Required(ErrorMessage = "{0} boş geçilemez")]
+        public bool IsActive { get; set; }
+    }
+}
